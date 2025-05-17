6451eca473f41eacd5f6a660d926a4a0dcb3f468
Moshe Dayan <xynpyro@gmail.com>
2025-05-17 18:39:10 +0700

    feat : added google_fonts, shared theme, color and fontstyle

---
+++ lib/shared/theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 32.0;
+double defaultRadius = 12.0;
+
+Color kPrimaryColor = const Color(0xff9F9CCC);
+Color kPrimaryLightColor = const Color(0xffECECFA);
+Color kSecondaryColor = const Color(0xffEFABA7);
+Color kWhiteColor = const Color(0xffFFFFFF);
+Color kTriaryColor = const Color(0xffFCEEEC);
+Color kBlackColor = const Color(0xff30313E);
+Color kTeksColor = const Color(0xff663835);
+Color kRedDangerColor = const Color(0xffF32013);
+Color kGreenSafeColor = const Color(0xff00FF00);
+Color kTransparentColor = Colors.transparent;
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+TextStyle primaryLightTextStyle = GoogleFonts.poppins(
+  color: kPrimaryLightColor,
+);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: kSecondaryColor,
+);
+TextStyle whtieTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+TextStyle triaryTextStyle = GoogleFonts.poppins(
+  color: kTriaryColor,
+);
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+TextStyle teksTextStyle = GoogleFonts.poppins(
+  color: kTeksColor,
+);
+TextStyle redDangerTextStyle = GoogleFonts.poppins(
+  color: kRedDangerColor,
+);
+TextStyle greenSafeTextStyle = GoogleFonts.poppins(
+  color: kGreenSafeColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
